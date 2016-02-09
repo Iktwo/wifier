2560fa96aeb586a6863605de97c61d8924d0cca6
Isaac Salazar <iktwo.sh@gmail.com>
2016-02-08 20:00:49 -0800

    Initial implementation for expanding inner list

--- app/src/main/java/com/iktwo/wifier/adapter/AccessPointsAdapter.java
+++ app/src/main/java/com/iktwo/wifier/adapter/AccessPointsAdapter.java
@@ -1,7 +1,7 @@
 package com.iktwo.wifier.adapter;
 
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +11,7 @@ import android.widget.TextView;
 import com.iktwo.wifier.R;
 import com.iktwo.wifier.data.AccessPoint;
 import com.iktwo.wifier.data.WifiNetwork;
+import com.iktwo.wifier.utils.RecyclerItemClickListener;
 import com.iktwo.wifier.views.CustomLinearLayout;
 
 import java.util.List;
@@ -22,11 +23,15 @@ public class AccessPointsAdapter extends RecyclerView.Adapter<AccessPointsAdapte
     private List<AccessPoint> items;
     private int mItemLayout;
     private int mGroupLayout;
+    private int mExpandedIndex = -1;
 
-    public AccessPointsAdapter(List<AccessPoint> items, int itemLayout, int groupLayout) {
+    private RecyclerItemClickListener.OnItemClickListener mOnItemClickListener;
+
+    public AccessPointsAdapter(List<AccessPoint> items, int itemLayout, int groupLayout, RecyclerItemClickListener.OnItemClickListener listener) {
         this.items = items;
         mItemLayout = itemLayout;
         mGroupLayout = groupLayout;
+        mOnItemClickListener = listener;
     }
 
     @Override
@@ -47,7 +52,7 @@ public class AccessPointsAdapter extends RecyclerView.Adapter<AccessPointsAdapte
                 break;
         }
 
-        return new ViewHolder(v);
+        return new ViewHolder(v, mOnItemClickListener);
     }
 
     @Override
@@ -58,11 +63,16 @@ public class AccessPointsAdapter extends RecyclerView.Adapter<AccessPointsAdapte
         holder.ssid.setText(firstNetwork.getSsid());
 
         if (ap.length() > 1) {
+            if (position == mExpandedIndex) {
+                holder.innerRecyclerView.setVisibility(View.VISIBLE);
+            } else {
+                holder.innerRecyclerView.setVisibility(View.GONE);
+            }
+
             /// TODO: here initialize adapter and stuff
             holder.innerRecyclerView.setHasFixedSize(true);
             holder.innerRecyclerView.setLayoutManager(new CustomLinearLayout(holder.itemView.getContext()));
             holder.innerRecyclerView.setItemAnimator(new DefaultItemAnimator());
-
             holder.innerRecyclerView.setAdapter(new WifiNetworksAdapter(ap.getWifiNetworks(), R.layout.delegate_wifi_network));
         } else {
             holder.bssid.setText(firstNetwork.getBssid());
@@ -108,17 +118,42 @@ public class AccessPointsAdapter extends RecyclerView.Adapter<AccessPointsAdapte
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        private final RecyclerItemClickListener.OnItemClickListener mListener;
+        private final View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (innerRecyclerView != null) {
+                    if (mExpandedIndex != getAdapterPosition()) {
+                        mExpandedIndex = getAdapterPosition();
+                    } else {
+                        mExpandedIndex = -1;
+                    }
+
+                    notifyItemChanged(getAdapterPosition());
+                } else {
+                    mListener.onItemClick(view, getAdapterPosition());
+                }
+            }
+        };
+
+        public CardView cardView;
         public TextView ssid;
         public TextView bssid;
         public TextView manufacturer;
         public RecyclerView innerRecyclerView;
 
-        public ViewHolder(View view) {
+        public ViewHolder(View view, RecyclerItemClickListener.OnItemClickListener listener) {
             super(view);
+
+            mListener = listener;
+
             ssid = (TextView) view.findViewById(R.id.text_view_ssid);
             bssid = (TextView) view.findViewById(R.id.text_view_bssid);
             manufacturer = (TextView) view.findViewById(R.id.text_view_manufacturer);
             innerRecyclerView = (RecyclerView) view.findViewById(R.id.inner_recycler_view);
+            cardView = (CardView) view.findViewById(R.id.card_view);
+
+            cardView.setOnClickListener(onClickListener);
         }
     }
 }
--- app/src/main/java/com/iktwo/wifier/fragment/MainFragment.java
+++ app/src/main/java/com/iktwo/wifier/fragment/MainFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +14,7 @@ import com.iktwo.wifier.R;
 import com.iktwo.wifier.adapter.AccessPointsAdapter;
 import com.iktwo.wifier.data.AccessPoint;
 import com.iktwo.wifier.data.WifiNetwork;
+import com.iktwo.wifier.utils.RecyclerItemClickListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -53,7 +55,13 @@ public class MainFragment extends Fragment {
         mRecyclerView.setItemAnimator(new DefaultItemAnimator());
 
         mAccessPointsAdapter = new AccessPointsAdapter(mAccessPoints,
-                R.layout.delegate_wifi_network, R.layout.delegate_wifi_networks);
+                R.layout.delegate_wifi_network,
+                R.layout.delegate_wifi_networks, new RecyclerItemClickListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Log.d(TAG, "clicked " + view);
+            }
+        });
 
         mRecyclerView.setAdapter(mAccessPointsAdapter);
 
